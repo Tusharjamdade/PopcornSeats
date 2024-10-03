fd805a408b8975ba5ee44a0274ab8a07244244eb
Tusharjamdade <tusharnjamdade@gmail.com>
2024-10-03 16:26:05 +0000

    Created Schema

---
+++ prisma/migrations/20241003145530_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "director" TEXT NOT NULL,
+    "time" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" BYTEA NOT NULL,
+    "seats" INTEGER[],
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
